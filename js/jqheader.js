ffbca18c66d78364149a690049cda2626b2fc24f
KalleTHansen <kalleh98@gmail.com>
2018-05-30 12:39:36 +0200

    Fixed link names for validation. External js (jqheader)

---
+++ js/jqheader.js
@@ -0,0 +1,74 @@
+$( document ).ready(function() {
+
+$( "#cross" ).hide();//Luk menu knap
+$( ".menu" ).hide();//Menu
+$( ".search" ).hide();//Søgefelt
+$( "#closesearch" ).hide();//Luk søg knap
+
+$( "#hamburger" ).click(function() { //Åbne menu
+  $( ".search" ).hide();
+  $( "#hamburger" ).hide();
+  $( "#cross" ).show();
+
+  if ( $("#searchicon").css('display') == 'none' ){//Søgeikon er usynlig, søgekryds er synlig
+    $( "#searchicon" ).show();
+    $( "#closesearch" ).hide();
+  }
+$( ".menu" ).slideToggle( "fast", function() {
+});
+});
+
+$( "#cross" ).click(function() { //Lukke menu
+  $( "#cross" ).hide();
+  $( "#hamburger" ).show();
+$( ".menu" ).slideToggle( "fast", function() {
+});
+});
+
+$( "#searchicon" ).click(function() { //Klikke på søgeicon
+  $( ".menu" ).hide();
+  $( "#searchicon" ).hide();
+  $( "#closesearch" ).show();
+
+  if ( $("#hamburger").css('display') == 'none' ){// Hamburger er usynlig, menukryds er synlig
+    $( "#hamburger" ).show();
+    $( "#cross" ).hide();
+  }
+$( ".search" ).slideToggle( "fast", function() {
+});
+});
+
+$( "#closesearch" ).click(function() { //Klikke på luk søgefunktion
+  $( "#closesearch" ).hide();
+  $( "#searchicon" ).show();
+  $( "#hamburger" ).show();
+  $( ".search" ).slideToggle( "fast", function() {
+  });
+});
+
+
+
+var $window = $(window);
+
+function checkWidth() {
+    var windowsize = $window.width();
+    if (windowsize >= 950) {
+        //if the window is greater than or equal to 950px wide then show menu.
+        $( ".menu" ).show();//Menu;
+        $( ".search" ).show();//Search;
+    }
+    else {
+        //if the window is less than 950px wide then hide menu.
+        $( ".menu" ).hide();//Menu;
+        $( "#cross" ).hide();//Luk menu knap
+        $( "#hamburger" ).show();
+        $( ".search" ).hide();//Search bar;
+        $( "#searchicon" ).show();
+        $( "#closesearch" ).hide();
+    }
+}
+// Execute on load
+checkWidth();
+// Bind event listener
+$(window).resize(checkWidth);
+});
