42a6f032c9b0688fbf92ce413cd2c9248678054c
KalleTHansen <kalleh98@gmail.com>
2018-06-03 21:36:45 +0200

    Gave up on splitting and jQ. Selected first half via css

--- js/accordion.js
+++ js/accordion.js
@@ -1,9 +1,22 @@
 $( document ).ready(function() {
-  $(".accordion").accordion({
+  var $accordions = $( ".accordion" ).accordion({
     header: "h3",//Angiver hvilket element, der skal tildeles styling som fane
     collapsible: true,//Så alle sektioner kan være lukkede samtidigt
     active: false,//Default skal alle faner være lukket.
     heightStyle: "content", //Tilpasser højden af det enkelte vindue til indholdet
     animate: {duration: 200} //Tiden i millisekunder det tager at åbne/lukke sektion
   });
+
+
+
+//  $("#accordion1").accordion({
+//    beforeActivate: .accordion('activate', false) {
+  //    accordions.not(this).accordion('option', 'active', false);
+//    });
+
+//  $( ".accordion" ).on( "accordionbeforeactivate", function( 'activate', false ) {} );
 });
+
+/*$( "#accordion1" ).click(function() {
+        $("#accordion2").accordion('activate', false);
+ });*/
